397cae0b201254700500a9a9695577358a963c17
AlexisDoroszkiewicz <aledoro@orange.fr>
2022-06-17 09:07:38 +0200

    proper memo

--- src/lib/useFilter.js
+++ src/lib/useFilter.js
@@ -1,14 +1,49 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function useFilter(data, filter) {
 	const [tasks, setTasks] = useState();
-	useMemo(() => {
-		data = data.filter((item) =>
-			filter != null
-				? item.selection.status == filter
-				: item.selection.status != "closed"
-		);
-		setTasks(data);
+	const noClosedStatus = useMemo(() => {
+		return data.filter((item) => item.selection.status != "closed");
+	}, [data]);
+
+	const newStatus = useMemo(() => {
+		return data.filter((item) => item.selection.status == "new");
+	}, [data]);
+
+	const ongoingStatus = useMemo(() => {
+		return data.filter((item) => item.selection.status == "ongoing");
+	}, [data]);
+
+	const readyStatus = useMemo(() => {
+		return data.filter((item) => item.selection.status == "ready");
+	}, [data]);
+
+	const closedStatus = useMemo(() => {
+		return data.filter((item) => item.selection.status == "closed");
+	}, [data]);
+
+	useEffect(() => {
+		switch (filter) {
+			case null:
+				setTasks(noClosedStatus);
+				break;
+
+			case "new":
+				setTasks(newStatus);
+				break;
+
+			case "ongoing":
+				setTasks(ongoingStatus);
+				break;
+
+			case "ready":
+				setTasks(readyStatus);
+				break;
+
+			case "closed":
+				setTasks(closedStatus);
+				break;
+		}
 	}, [filter]);
 
 	return tasks;
