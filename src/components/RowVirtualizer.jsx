be332fb87b0cfdbcfdb81c08a009ede2c3753ad3
AlexisDoroszkiewicz <aledoro@orange.fr>
2022-07-11 08:10:11 +0200

    no strict mode

--- src/components/RowVirtualizer.jsx
+++ src/components/RowVirtualizer.jsx
@@ -25,7 +25,7 @@ export default function RowVirtualizer({ filter }) {
 						width: "100%",
 						position: "relative",
 					}}>
-					{rowVirtualizer.measurementsCache.map((virtualRow) => {
+					{rowVirtualizer.getVirtualItems().map((virtualRow) => {
 						return (
 							<div
 								key={virtualRow.index}
