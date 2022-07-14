02a61bfd2865a66b28b52d972dda108057dcfdb6
AlexisDoroszkiewicz <aledoro@orange.fr>
2022-07-14 11:40:43 +0200

    removed unecessary markup

--- src/components/RowVirtualizer.jsx
+++ src/components/RowVirtualizer.jsx
@@ -1,12 +1,9 @@
 import Task from "@components/Task";
 import data from "@public/tasks.json";
 import { useWindowVirtualizer } from "@tanstack/react-virtual";
-import { useRef } from "react";
 import useFilter from "@lib/useFilter";
 
 export default function RowVirtualizer({ filter }) {
-	const parentRef = useRef();
-
 	const tasks = useFilter(data, filter);
 
 	const rowVirtualizer = useWindowVirtualizer({
@@ -17,30 +14,28 @@ export default function RowVirtualizer({ filter }) {
 
 	return (
 		<>
-			<div ref={parentRef} className="List">
-				<div
-					style={{
-						height: `${rowVirtualizer.getTotalSize()}px`,
-						width: "100%",
-						position: "relative",
-					}}>
-					{rowVirtualizer.getVirtualItems().map((virtualRow) => {
-						return (
-							<div
-								key={virtualRow.index}
-								style={{
-									position: "absolute",
-									top: 0,
-									left: 0,
-									width: "100%",
-									height: `${virtualRow.size}px`,
-									transform: `translateY(${virtualRow.start}px)`,
-								}}>
-								<Task task={tasks[virtualRow.index]} />
-							</div>
-						);
-					})}
-				</div>
+			<div
+				style={{
+					height: `${rowVirtualizer.getTotalSize()}px`,
+					width: "100%",
+					position: "relative",
+				}}>
+				{rowVirtualizer.getVirtualItems().map((virtualRow) => {
+					return (
+						<div
+							key={virtualRow.index}
+							style={{
+								position: "absolute",
+								top: 0,
+								left: 0,
+								width: "100%",
+								height: `${virtualRow.size}px`,
+								transform: `translateY(${virtualRow.start}px)`,
+							}}>
+							<Task task={tasks[virtualRow.index]} />
+						</div>
+					);
+				})}
 			</div>
 		</>
 	);
